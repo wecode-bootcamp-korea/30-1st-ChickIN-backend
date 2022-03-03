4bb439adedaf1730383384c6c71ab2316eb889f0
김가람휘 <kimkaramhwi@gimgalamhwiui-MacBookPro.local>
2022-03-03 19:54:55 +0900

    Modify: Modeling, csv

--- chickin/settings.py
+++ chickin/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib     import Path
 from my_settings import DATABASES, SECRET_KEY
+
+import pymysql
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
