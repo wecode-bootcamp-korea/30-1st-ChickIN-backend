7ff34ebd7dc7b7e1e7fca6700250c49a1a806ecb
김가람휘 <kimkaramhwi@gimgalamhwiui-MacBookPro.local>
2022-03-03 19:59:16 +0900

    Modify: Modeling, csv

--- chickin/urls.py
+++ chickin/urls.py
@@ -13,11 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
-#    path('users', include('users.urls')),
-    path('products', include('products.urls')),
-#    path('carts', include('carts.urls')),
-#    path('orders', include('orders.urls')),
+
 ]
