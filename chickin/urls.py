c84bec4e7a7f70a60bce1e2d5b2f986d6b2249b6
jayce-kim1123 <kimjisung777@gmail.com>
2022-03-04 15:28:39 +0900

    feat: urls

--- chickin/urls.py
+++ chickin/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 
 urlpatterns = [
+    path('carts',    include('carts.urls')),
+    path('orders',   include('orders.urls')),
     path('products', include('products.urls')),
-    path('users', include('users.urls')),
+    path('users',    include('users.urls')),
 ]
