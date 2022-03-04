c21a12dc8dac2e76005acc0ae86adf51c6b4cf95
김가람휘 <kimkaramhwi@gimgalamhwiui-MacBookPro.local>
2022-03-04 18:22:57 +0900

    Modify: productdetailview

--- products/views.py
+++ products/views.py
@@ -1,24 +1,23 @@
 from django.http  import JsonResponse
 from django.views import View
 
-from products.models import Product, ProductImage, Option
+from products.models import Product, Option, ProductOption
 
 class ProductDetailView(View):
     def get(self, request, product_id):
         try:
-            product = Product.objects.get(id=product_id)
-            images  = product.productimage_set.all()
-            options = product.productoption_set.all()
+            product = Product.objects.prefetch_related('productimage_set', 'productoption__option')\
+                .get(id=product_id)
 
             data = {
-                    'image'       : [image.image_url for image in images],
+                    'image'       : [image.image_url for image in product.productimage_set.all()],
                     'name'        : product.name,
                     'price'       : product.price,
                     'description' : product.description,
-                    'option'      : {
-                        'name'  : [option.name for option in options],
-                        'price' : [option.price for option in options]
-                    }
+                    'option'      : [{
+                        'name'  : option.name,
+                        'price' : option.price
+                    } for option in product.productoption__option.all()]
                 }
 
             return JsonResponse({'data':data}, status=200)
