5a81466f3a1509010f831326916b307681501bf5
김가람휘 <kimkaramhwi@gimgalamhwiui-MacBookPro.local>
2022-03-04 19:29:26 +0900

    Modify: productlistview

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from products.views import ProductListView
+from products.views import ProductDetailView
 
 urlpatterns = [
-    path('', ProductListView.as_view())
+    path('/<int:product_id>', ProductDetailView.as_view())
 ]
