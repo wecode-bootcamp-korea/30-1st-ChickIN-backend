aa9c2e0823f05654d58a4100268f3c3bf50cedf8
김가람휘 <kimkaramhwi@gimgalamhwiui-MacBookPro.local>
2022-03-06 18:35:57 +0900

    Modify: productlistview

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.http      import JsonResponse
 from django.views     import View
 from django.db.models import Q
 
-from products.models import Product, SubCategory
+from products.models import Product
 
 class ProductListView(View):
     def get(self, request):
@@ -10,47 +10,33 @@ class ProductListView(View):
             main_category = request.GET.get('main_category', None)
             sub_category  = request.GET.get('sub_category', None)
             searching     = request.GET.get('name', None)
-            sort          = request.GET.get('sort', None)
+            sort          = request.GET.get('sort', '0')
             limit         = int(request.GET.get('limit', 36))
             offset        = int(request.GET.get('offset', 0))
             product_list  = []
             
             q = Q()
             
-            products = Product.objects.all()
-            products = SubCategory.objects.prefetch_related('product_set').filter(main_category_id=main_category)\
-        	   if main_category else products
+            if main_category:
+                q &= Q(sub_category__main_category_id=main_category)
             if sub_category:
                 q &= Q(sub_category_id=sub_category)
             if searching:
                 q &= Q(name__icontains=searching)
-            products = Product.objects.select_related('sub_category').filter(q)\
-                if sub_category or searching else products     
-            if sort:
-                sort_type = {
-                    '0' : '?',
-                    '1' : '-price',
-                    '2' : 'price'
-                }
-                products = products.order_by(sort_type[sort])
-            products = products[offset:offset+limit]
+            sort_type = {
+                '0' : 'id',
+                '1' : '?',
+                '2' : '-price',
+                '3' : 'price'
+            }
+            products = Product.objects.select_related('sub_category')\
+                .filter(q).order_by(sort_type[sort])[offset:offset+limit]
 
-            if main_category:
-                for product in products:
-                    product = product.product_set.all()
-                    for item in product:
-                        product_list.append({
-                            'thumbnail'   : item.thumbnail,
-                            'name'        : item.name,
-                            'price'       : item.price
-                        })
-
-            else:
-                product_list = [{
-                            'thumbnail' : product.thumbnail,
-                            'name'      : product.name,
-                            'price'     : product.price
-                        } for product in products]
+            product_list = [{
+                        'thumbnail' : product.thumbnail,
+                        'name'      : product.name,
+                        'price'     : product.price
+                    } for product in products]
 
             return JsonResponse({'products_list':product_list}, status=200)
 
