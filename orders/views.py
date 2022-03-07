f23ed571ec3356f7f256f93913176b8658944caa
김가람휘 <kimkaramhwi@gimgalamhwiui-MacBookPro.local>
2022-03-07 21:47:16 +0900

    Add: OrderView-patch

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import json, uuid
 
-# Create your views here.
+from django.http      import JsonResponse
+from django.views     import View
+from django.db        import transaction
+from django.db.models import F, Sum
+from enum             import Enum
+
+from users.utils   import login_required
+from users.models  import User
+from carts.models  import Cart
+from orders.models import Order, OrderItem, OrderOption, OrderStatus
+
+class OrederStatusEnum(Enum):
+    PAID            = 1
+    PREPARING       = 2
+    SHIPPEND        = 3
+    DELIVERD        = 4
+    ORDER_CANCELLED = 5
+
+class OrderView(View):
+    @login_required
+    def patch(self, request):
+        try:
+            data = json.loads(request.body)
+
+            with transaction.atomic():
+                Order.objects.filter(id=data['order_id']).update(order_status=OrederStatusEnum.ORDER_CANCELLED.value)
+                return JsonResponse({'message':'SUCCESS'}, status=200)
+
+        except Order.DoesNotExist:
+            return JsonResponse({'message':'NOT_FOUND'}, status=404)
+
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
