5d7edc9fbd9003853dacbbac0e347be5614a9d02
jayce-kim1123 <kimjisung777@gmail.com>
2022-03-07 17:56:24 +0900

    feature/cart_read

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http      import JsonResponse
+from django.views     import View
+
+from users.utils      import login_required
+from carts.models     import Cart
+
+class CartView(View):
+    @login_required
+    def get(self, request):
+        carts = Cart.objects.filter(user_id = request.user.id)
+        result = [{
+                'cart_id'    : cart.id,
+                'user_id'    : request.user.id,
+                'quantity'   : cart.quantity,
+                'product'    :{
+                    'name'       : cart.product.name,
+                    'thumbnail'  : cart.product.thumbnail,
+                    'price'      : int(cart.product.price),
+                    },
+                'option'     : [{
+                    'name'  : cart_option.option.name,
+                    'price' : cart_option.option.price,
+                    }for cart_option in cart.cartoption_set.all()],
+                } for cart in carts]
+
+        return JsonResponse({'result' : result}, status=200)
--- chickin/urls.py
+++ chickin/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
+from django.urls import path,include
 
 urlpatterns = [
+    path('carts',    include('carts.urls')),
     path('products', include('products.urls')),
-    path('users', include('users.urls')),
+    path('users',    include('users.urls')),
 ]
