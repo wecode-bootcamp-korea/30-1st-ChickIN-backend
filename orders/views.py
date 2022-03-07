fd86cb93683624518ced62e43ae05913dd5c81aa
강경훈 <kyunghun@gang-gyeonghuns-MacBook-Pro.local>
2022-03-07 17:09:07 +0900

    Mentor review

--- orders/views.py
+++ orders/views.py
@@ -21,39 +21,57 @@ class OrederStatusEnum(Enum):
 class OrderView(View):
     @login_required
     def post(self, request):
-        data     = json.loads(request.body)
-        user     = request.user
-        cart_ids = data['cart_ids']
-        carts    = Cart.objects.filter(id__in=cart_ids, user=user)
-
         try:
+            data     = json.loads(request.body)
+            user     = request.user
+            cart_ids = data['cart_ids']
+            carts    = Cart.objects.filter(id__in=cart_ids, user=user)
+            
             if not carts.exists():
                 return JsonResponse({'message':'NOT_FOUND'}, status=404)
 
             with transaction.atomic():
                 order = Order.objects.create(
                     order_number = uuid.uuid4(),
-                    address      = User.objects.get(id=user.id).address,
+                    address      = user.address,
                     user         = user,
-                    order_status = OrderStatus.objects.get(id=OrederStatusEnum.PREPARING.value)
+                    order_status = OrederStatusEnum.PREPARING.value
                 )
 
-                order_items = [OrderItem(
-                    order      = order,
-                    product    = cart.product,
-                    quantity   = cart.quantity 
-                )for cart in carts]
-                OrderItem.objects.bulk_create(order_items)
-
-                cart_options = CartOption.objects.filter(cart__in=cart_ids)
-                order_options = [OrderOption(
-                    option     = cart_option.option,
-                    order_item = OrderItem.objects.get(order_id=order)
-                ) for cart_option in cart_options]
+                for cart in carts:
+                    order_item = OrderItem.objects.create(
+                        order      = order,
+                        product    = cart.product,
+                        quantity   = cart.quantity
+                    ) 
+                                    
+                    cart_options  = cart.cartoption_set.all()
+                    order_options = [OrderOption(
+                        option     = cart_option.option,
+                        order_item = order_item
+                    ) for cart_option in cart_options]
+                
+                    OrderOption.objects.bulk_create(order_options)
+                
                 carts.delete()
-                OrderOption.objects.bulk_create(order_options)
-
-            return JsonResponse({'message':order.id}, status=201)
+                response = {
+                    "message" : "created",
+                    "data" : {
+                        "order_id" : order.id,
+                        "order_number" : order.order_number
+                    }
+                }
+            return JsonResponse(response, status=201)
 
         except transaction.TransactionManagementError:
-            return JsonResponse({'message':'TransactionManagementError'}, status=401)  
+            return JsonResponse({'message':'TransactionManagementError'}, status=400)  
+
+class OrderNowView(View):
+    def post(self, request):
+        """
+        즉시 구매
+        
+        주의 사항
+        1. cart 정보가 없다
+        2. product_id랑 option의 정보를 직접 받아서 구현
+        """
