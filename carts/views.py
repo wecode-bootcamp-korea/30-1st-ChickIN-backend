701e2108257bee0e73cf36bd881ad8514ad212f4
jayce-kim1123 <kimjisung777@gmail.com>
2022-03-08 21:32:15 +0900

    feature/carts_patch

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import json
+from statistics import quantiles
 
-# Create your views here.
+from django.http      import JsonResponse
+from django.views     import View
+
+from users.utils      import login_required
+from carts.models     import Cart
+
+class CartView(View):
+    @login_required
+    def patch(self, request):
+        try:
+            data     = json.loads(request.body)
+            user     = request.user.id
+            cart_id  = data['cart_id']
+            quantity = data["quantity"]
+            cart     = Cart.objects.get(id=cart_id, user_id=user)
+
+            cart.quantity = quantity
+            cart.save()
+
+            return JsonResponse({"message":"SUCCESS"}, status=201)
+        except KeyError:
+            return JsonResponse({"message":"KEY ERROR"}, status=401)
