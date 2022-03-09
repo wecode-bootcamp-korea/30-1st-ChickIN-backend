1dae243898fdc9ab6a2e0b595195ce4bc9d96cce
jayce-kim1123 <kimjisung777@gmail.com>
2022-03-09 16:16:55 +0900

    modify/carts_add

--- carts/views.py
+++ carts/views.py
@@ -10,6 +10,26 @@ from products.models  import Option
 
 class CartView(View):
     @login_required
+    def get(self, request):
+        carts = Cart.objects.filter(user_id = request.user.id)
+        result = [{
+                'cart_id'    : cart.id,
+                'user_id'    : request.user.id,
+                'quantity'   : cart.quantity,
+                'product'    :{
+                    'name'       : cart.product.name,
+                    'thumbnail'  : cart.product.thumbnail,
+                    'price'      : int(cart.product.price),
+                    },
+                'option'     : [{
+                    'name'  : cart_option.option.name,
+                    'price' : cart_option.option.price,
+                    }for cart_option in cart.cartoption_set.all()],
+                } for cart in carts]
+        
+        return JsonResponse({'result' : result}, status=200)
+    
+    @login_required
     def post(self, request):
         try:
             data       = json.loads(request.body)
