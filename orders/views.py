4154ac408aa09a1e0a6aba6975b933e956dc340d
김가람휘 <kimkaramhwi@gimgalamhwiui-MacBookPro.local>
2022-03-09 17:16:48 +0900

    Modify: orderspatch

--- orders/views.py
+++ orders/views.py
@@ -3,15 +3,14 @@ import json, uuid
 from django.http      import JsonResponse
 from django.views     import View
 from django.db        import transaction
-from django.db.models import F, Sum
+from json             import JSONDecodeError
 from enum             import Enum
 
 from users.utils   import login_required
-from users.models  import User
 from carts.models  import Cart
-from orders.models import Order, OrderItem, OrderOption, OrderStatus
+from orders.models import Order, OrderItem, OrderOption
 
-class OrederStatusEnum(Enum):
+class OrderStatusEnum(Enum):
     PAID            = 1
     PREPARING       = 2
     SHIPPEND        = 3
@@ -20,13 +19,26 @@ class OrederStatusEnum(Enum):
 
 class OrderView(View):
     @login_required
-    def patch(self, request):
+    def patch(self, request, order_id):
         try:
-            data = json.loads(request.body)
+            data   = json.loads(request.body)
+            user   = request.user
+            status = data['status']
 
-            with transaction.atomic():
-                Order.objects.filter(id=data['order_id']).update(order_status=OrederStatusEnum.ORDER_CANCELLED.value)
-                return JsonResponse({'message':'SUCCESS'}, status=200)
+            order_status = {
+                'paid'            : OrderStatusEnum.PAID.value,
+                'preparing'       : OrderStatusEnum.PREPARING.value,
+                'shippend'        : OrderStatusEnum.SHIPPEND.value,
+                'deliverd'        : OrderStatusEnum.DELIVERD.value,
+                'order_cancelled' : OrderStatusEnum.ORDER_CANCELLED.value
+            }
+
+            order = Order.objects.get(user=user, order_id=order_id)
+
+            order.order_status = order_status[status]
+            order.save()
+
+            return JsonResponse({'message':'SUCCESS'}, status=200)
 
         except Order.DoesNotExist:
             return JsonResponse({'message':'NOT_FOUND'}, status=404)
