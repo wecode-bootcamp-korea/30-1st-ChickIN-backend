3e01c3cecbad6530ab907685d52e7546bc753994
김가람휘 <kimkaramhwi@gimgalamhwiui-MacBookPro.local>
2022-03-09 17:13:30 +0900

    Modify: ordersget

--- orders/views.py
+++ orders/views.py
@@ -3,7 +3,6 @@ import json, uuid
 from django.http      import JsonResponse
 from django.views     import View
 from django.db        import transaction
-from django.db.models import F, Sum
 from enum             import Enum
 
 from users.utils   import login_required
@@ -15,23 +14,12 @@ class OrderView(View):
     @login_required
     def get(self, request):
         user          = request.user
-        order         = request.GET.get('id',)
-        order_items   = OrderItem.objects.filter(order_id=order)
-        order_options = OrderOption.objects.filter(order_item__order_id=order)
-
-        if order_options:
-            total_price   = int(Order.objects.filter(id=order)\
-                            .annotate(total=Sum(F('orderitem__product__price')*F('orderitem__quantity')\
-                                +F('orderitem__orderoption__option__price')))[0].total)
-                                
-        else:
-            total_price   = int(Order.objects.filter(id=order)\
-                            .annotate(total=Sum(F('orderitem__product__price')*F('orderitem__quantity')))[0].total)
+        orders        = Order.objects.filter(user=user)
         
         order_list = [{
-            'order_id'   : order,
-            'user'       : User.objects.get(id=user.id).username,
-            'address'    : Order.objects.get(id=order).address,
+            'order_id'   : order.id,
+            'user'       : user.username,
+            'address'    : order.address,
             'order_items': [{
                 'product_id'    : order_item.product.id,
                 'quantity'      : order_item.quantity,
@@ -42,8 +30,7 @@ class OrderView(View):
                     'option_name'  : order_option.option.name,
                     'option_price' : order_option.option.price
                 } for order_option in order_item.orderoption_set.all()] 
-            }for order_item in order_items],
-            'total_price' : total_price
-        }]
+            }for order_item in order.orderitem_set.all()]
+        } for order in orders]
 
         return JsonResponse({'order_list':order_list}, status=200)
